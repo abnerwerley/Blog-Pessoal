17ebc5fb5b27380485ae0db3655daa90df9780ac
abnerwerley <abnerwerley77@gmail.com>
2021-09-27 12:16:22 -0300

    Repository e Controller de Usuario adicionados

--- src/main/java/br/com/generation/blogPessoal/model/Usuario.java
+++ src/main/java/br/com/generation/blogPessoal/model/Usuario.java
@@ -23,6 +23,38 @@ public class Usuario {
 	
 	@NotNull
 	private String senha;
+
+	public long getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(long id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 	
 	
 }
