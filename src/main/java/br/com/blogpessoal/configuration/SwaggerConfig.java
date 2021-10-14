a6e8b9081e802a9f2d03dca45b76283e7e88615e
abnerwerley <abnerwerley77@gmail.com>
2021-10-14 15:33:29 -0300

    [Feature Abner - Task: BlogPessoalApplication transformada em um EndPoint]

--- src/main/java/br/com/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/br/com/blogpessoal/configuration/SwaggerConfig.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
